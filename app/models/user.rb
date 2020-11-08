fd83db2fe7efb87b6cf00172c88b041052b4863f
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-08 22:41:11 +0000

    added validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, :given_name, :family_name, :email, presence: true
+  validates :username, :email, uniqueness: true
+  validates :given_name, :family_name , length: { maximum: 100,
+    too_long: "%{count} characters is the maximum allowed" }
+  validates :email, confirmation: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
