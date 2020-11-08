b3b83f26447c47d17e277dba1f08f615054f25dd
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-08 23:25:51 +0000

    created new user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "user attributes must not be empty" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:username].any?
+    assert user.errors[:given_name].any?
+    assert user.errors[:family_name].any?
+    assert user.errors[:email].any?
+  end
 end
